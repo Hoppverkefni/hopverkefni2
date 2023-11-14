dbf35110a1fd5c4026590ea728a53bec6bfe0237
karlpv <karlpv213@gmail.com>
2023-11-14 20:35:16 +0000

    grid start

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ fetch("https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=6")
   .then((data) => {
     const gridContainer = document.getElementById("grid-container");
 
-    data.items.forEach((item) => {
+    // Check if 'data' itself is an array or if it has an 'items' property that is an array
+    const items = Array.isArray(data) ? data : (data.items || []);
+
+    // Now iterate over 'items'
+    items.forEach((item) => {
       const gridItem = document.createElement("div");
       gridItem.classList.add("grid-item");
 
-      // Assuming 'item' has properties like 'title', 'image', 'price', 'category'
+      // Adjust the property names according to your actual API response
       gridItem.innerHTML = `
         <h3>${item.title}</h3>
-        <img src="${item.image}" alt="${item.title}" style="width:100%; height:auto;">
+        <img src="${item.image}" alt="${item.title}">
         <p>Price: ${item.price}</p>
         <p>Category: ${item.category}</p>
       `;
